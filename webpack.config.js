015e5648531abe4354f1e3ef1ad2acdc986fa218
abauruel <abauruel@gmail.com>
2019-12-12 08:25:42 -0300

    start configuration

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public")
+  },
+  modules: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "css-loader" }, { loader: "style-loader" }]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: "file-loader"
+        }
+      }
+    ]
+  }
+};
